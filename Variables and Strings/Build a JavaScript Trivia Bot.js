09abfb8d33eb3f701e33990840f91555c54cd8f4
Victor M. Tavárez <victortavarez23@gmail.com>
2026-04-01 12:08:23 -0400

    Variables and Strings — JavaScript First Exercises

---
+++ Variables and Strings/Build a JavaScript Trivia Bot.js
@@ -0,0 +1,23 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "Vottik";
+const botLocation = "Dominican Republic";
+const favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + "."); 
+
+let codingFact = favoriteLanguage + " was created in just 10 days by Brendan Eich while working at Netscape in May 1995.";
+
+console.log(codingFact);
+
+codingFact = "The language was initially called Mocha, then briefly LiveScript, before being renamed " + favoriteLanguage + " as a marketing strategy to capitalize on the popularity of the Java programming language";
+
+console.log(codingFact);
+
+codingFact = "It's not the same as Java: Despite the name, Java and " + favoriteLanguage + " are completely different languages. Java is a compiled, statically typed language, while JavaScript is an interpreted, dynamically typed scripting language.";
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation +".");
---
+++ Variables and Strings/Build a Sentence Maker.js
@@ -0,0 +1,21 @@
+let adjective = "ambitious";
+let noun = "platypus";
+let verb = "polka-dancing";
+let place = "disco-ball cathedral";
+let adjective2 = "glittery";
+let noun2 = "marshmallows";
+
+const firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "melancholy";
+noun = "golem";
+verb = "starlight";
+place = "forgotten observatory";
+adjective2 = "crystalline";
+noun2 = "weeping";
+
+const secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
